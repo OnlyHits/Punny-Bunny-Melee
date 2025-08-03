5363554efe700a9155bd4a45b2df84c1d208a97a
Gabriel <gabriel.medoukali@epitech.eu>
2025-08-03 18:11:21 +0200

    after compil

--- Assets/Src/Scripts/GMTK/LevelDesign/LevelDesign.cs
+++ Assets/Src/Scripts/GMTK/LevelDesign/LevelDesign.cs
@@ -1,9 +1,5 @@
 using UnityEngine;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 namespace GMTK
 {
     public class LevelDesign : MonoBehaviour
@@ -12,13 +8,11 @@ namespace GMTK
 
         public BoxCollider PlaneBoxCollider { get => m_planeBoxCollider; protected set { } }
 
-#if UNITY_EDITOR
         public Vector3 GetPlaneSize()
         {
             Vector3 size = m_planeBoxCollider.size;
             size.Scale(m_planeBoxCollider.transform.lossyScale);
             return size;
         }
-#endif
     }
 }
